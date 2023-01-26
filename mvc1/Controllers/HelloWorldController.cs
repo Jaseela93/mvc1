e4661bcc2d594b7fb21a24e6688839b32c22d74c
Jaseela93 <jaseelajamal93@gmail.com>
2023-01-26 10:30:30 +0400

    1

---
+++ mvc1/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mvc1.Models;
+
+namespace mvc1.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs= new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            //DogViewModel doggo = new DogViewModel() { Name="Sif", Age=2};
+            return View(dogs);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            //  return View("Index");
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+        public string Hello()
+        {
+            return "Who is there?";
+        }
+    }
+}
